79381e11f8e51a4678f3de83acd2110740c03d57
bafanabafana215 <gstockdale15@gmail.com>
2021-10-28 11:29:18 -0700

    First try with a personal access token

--- Testing.R
+++ Testing.R
@@ -1,5 +1,7 @@
 print("This project was created in R-Studio")
 print("Now it lives in GitHub")
-print("This is my second attempt")
+print("This is my second attempt to link in R")
+print("this is my first try with a personal access toekn")
+
 
 
